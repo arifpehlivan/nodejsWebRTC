1b53c935a47799e4b27fee5a09af56c604c99462
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-09-10 03:57:53 +0300

    webrtc

--- public/script.js
+++ public/script.js
@@ -7,12 +7,33 @@ const myPeer = new Peer(undefined, {
 
 const myVideo = document.createElement('video');
 myVideo.muted = true;
+const peers = {};
 
 navigator.mediaDevices.getUserMedia({
     video:true,
     audio:true
 }).then(stream =>{
     addVideoStream(myVideo,stream)
+    
+    myPeer.on('call', call =>{
+        call.answer(stream)
+        const video = document.createElement('video')
+
+        call.on('stream', userVideoStream =>{
+            addVideoStream(video, userVideoStream)
+
+        })
+    })
+
+    socket.on('user-connected', userId =>{
+        connectToNewUser(userId, stream)
+
+    })
+
+})
+
+socket.on('user-disconnected', userId =>{
+   if(peers[userId]) peers[userId].close()
 
 })
 
@@ -20,11 +41,20 @@ myPeer.on('open', id=>{
     socket.emit('join-room',ROOM_ID, id)
 })
 
-socket.emit('join-room', ROOM_ID, 10);
+function connectToNewUser(userId, stream){
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream =>{
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () =>{
+        video.remove()
+    })
 
-socket.on('user-connected', userId =>{
-    console.log('User Connected' + userId);
-});
+    peers[userId] = call
+}
+
+
 
 function addVideoStream(video, stream){
     video.srcObject = stream
--- server.js
+++ server.js
@@ -17,9 +17,12 @@ app.get('/:room', (req,res) =>{
 
 io.on('connection', socket =>{
     socket.on('join-room', (roomId, userId)=>{
-        socket.join(roomId);
-        socket.to(roomId).broadcast.emit('user-connected', userId);
-    });
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+        socket.on('disconnect',() =>{
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
+    })
 });
 
 server.listen(3000);
